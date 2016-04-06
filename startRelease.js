c72f87023e581cc9c8caabc33de6cef10bab53a9
wanghan <wh85125@163.com>
2016-04-06 15:26:45 +0800

    #test

--- startRelease.js
+++ startRelease.js
@@ -10,11 +10,11 @@ if (process.argv.length !== 3) {
 
 exec('git flow release start '+process.argv[2]);
 
-var fileData = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
+var fileData = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 console.log(fileData.version);
 fileData.version = process.argv[2];
 
-fs.writeFile('../package.json', JSON.stringify(fileData), function(err){
+fs.writeFile('./package.json', JSON.stringify(fileData), function(err){
     if(err){
         console.log('release branch '+ process.argv[2]+' version update failed');
         return;
